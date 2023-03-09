e363d9d89691d2ccd0d7b222cd5e1ff84afe9e1f
layred <layred.dota2@mail.ru>
2023-03-10 00:47:38 +0300

    change prod-settings

--- compose/django/start-django.prod.sh
+++ compose/django/start-django.prod.sh
@@ -7,6 +7,6 @@ set -o nounset
 cd src/
 
 poetry run python manage.py migrate
-poetry run gunicorn --access-logfile ./logs/gunicorn.log --workers 4 --bind 0.0.0.0:8000 --worker-class gevent --timeout 600 --log-level=debug core.wsgi:application
+poetry run gunicorn --access-logfile ./logs/gunicorn.log --workers 4 --bind 0.0.0.0:9010 --worker-class gevent --timeout 600 --log-level=debug core.wsgi:application
 
 cd ..
--- compose/django/start-django.sh
+++ compose/django/start-django.sh
@@ -7,6 +7,6 @@ set -o nounset
 cd src/
 
 poetry run python manage.py migrate
-poetry run python manage.py runserver 0.0.0.0:8000
+poetry run python manage.py runserver 0.0.0.0:9010
 
 cd ..
